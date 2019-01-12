d68fbabda1c1977ebd07e48a515b28bcc23f6995
Matiki <matiasbruzza@gmail.com>
2019-01-11 17:45:14 -0800

    added code reading and cleaning data

---
+++ analysis.R
@@ -0,0 +1,50 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tm)
+library(stringi)
+
+# Download Data Files
+if (!file.exists("Coursera-SwiftKey.zip")){
+        URL <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+        download.file(URL, 
+                      destfile = "Coursera-SwiftKey.zip", 
+                      method = "curl", 
+                      quote = "")
+        rm(URL)
+}
+
+# Unzip Data
+if(!file.exists("./final")){
+        unzip("Coursera-SwiftKey.zip")
+}
+
+# Create function to read the data
+readdata <- function(x){
+        readLines(paste("./final/en_US/", x, sep = ""),
+                  encoding = "UTF-8",
+                  skipNul = TRUE)
+}
+
+# Create function to sample the data
+textSample <- function(temp){
+        sample(temp, length(temp) * .01)
+}
+
+# Use functions to read and sample the data
+set.seed(12345)
+
+blog <- readdata("en_US.blogs.txt") %>% textSample()
+news <- readdata("en_US.news.txt") %>% textSample()
+twitter <- readdata("en_US.twitter.txt") %>% textSample()
+
+# Create a corpus and clean it up
+corpus <- c(blog, news, twitter) %>% 
+        VectorSource() %>%
+        VCorpus() %>%
+        tm_map(content_transformer(tolower)) %>%
+        tm_map(removePunctuation) %>%
+        tm_map(removeNumbers) %>%
+        tm_map(stripWhitespace)
+
+rm(list = c("blog", "news", "twitter", "textSample", "readdata"))
